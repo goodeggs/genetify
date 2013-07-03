633230348e60059b2f2c6480d3717b357fb8d6d2
Bob Zoller <bob@zoller.us>
2013-07-03 14:39:36 -0700

    use ENV variable DATABASE_URL to set up database config

--- genetify/CONFIG.php
+++ genetify/CONFIG.php
@@ -1,9 +1,9 @@
 <?php
-
-$DB['host'] = 'localhost';
-$DB['user'] = 'genetify_user';
-$DB['password'] = '';
-$DB['database'] = 'genetify';
+$parts = parse_url($_ENV['DATABASE_URL']);
+$DB['host'] = $parts['host'];
+$DB['user'] = $parts['user'];
+$DB['password'] = $parts['pass'];
+$DB['database'] = trim($_ENV['path'], '/');
 
 $CONFIG['browscap'] = false; // browscap is defunct, turn off by default if & until a replacement is installed
 
